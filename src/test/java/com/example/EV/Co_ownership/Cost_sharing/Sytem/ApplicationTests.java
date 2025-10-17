fa757c034e746deff922166441f7f2cea00f2f6c
voquoctuan2409 <tuan240920041506@gmail.com>
2025-10-17 19:05:18 +0700

    upload payment-costsharing-service

---
+++ src/test/java/com/example/EV/Co_ownership/Cost_sharing/Sytem/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EV.Co_ownership.Cost_sharing.Sytem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
