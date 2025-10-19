477512eee1f19329cba52ae443de4a3823ae4655
TaiyouDang <kiritanitaiyo@gmail.com>
2025-10-19 14:55:33 +0700

    Add api update booking

---
+++ booking-usage-service/src/booking/dto/update-booking.dto.ts
@@ -0,0 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsOptional, IsString, IsDateString } from 'class-validator';
+import { BookingStatus } from '../booking.entity';
+
+export class UpdateBookingDto {
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsDateString()
+  booking_date?: Date;
+
+  @ApiProperty({ example: '08:00', required: false })
+  @IsOptional()
+  @IsString()
+  start_time?: string;
+
+  @ApiProperty({ example: '10:00', required: false })
+  @IsOptional()
+  @IsString()
+  end_time?: string;
+
+  @ApiProperty({ enum: BookingStatus, required: false })
+  @IsOptional()
+  @IsEnum(BookingStatus)
+  booking_status?: BookingStatus;
+
+  @ApiProperty({ example: 'Người dùng hủy do thay đổi kế hoạch', required: false })
+  @IsOptional()
+  @IsString()
+  cancel_reason?: string;
+}
