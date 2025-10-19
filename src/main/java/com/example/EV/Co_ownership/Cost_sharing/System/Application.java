52f46ad215ddf46c5982f71da9f23cd87914d2ac
voquoctuan2409 <tuan240920041506@gmail.com>
2025-10-19 17:14:38 +0700

    upload payment-costsharing-service

---
+++ src/main/java/com/example/EV/Co_ownership/Cost_sharing/System/Application.java
@@ -0,0 +1,13 @@
+package com.example.EV.Co_ownership.Cost_sharing.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
