9d47acecb21c4e74d1abe7a9390928c929f7b1f0
Võ Tá Duy <voduystkt@gmail.com>
2025-10-19 17:13:20 +0700

    create generateReport function

--- history-analytics-service/src/main/java/com/TaDuy/microservices/history_analytics_service/Service/GenimiService.java
+++ history-analytics-service/src/main/java/com/TaDuy/microservices/history_analytics_service/Service/GenimiService.java
@@ -1,4 +1,33 @@
 package com.TaDuy.microservices.history_analytics_service.Service;
 
-public class GenimiService {
+
+import com.TaDuy.microservices.history_analytics_service.Config.GenimiConfig;
+import com.TaDuy.microservices.history_analytics_service.Service.Imp.GenimiServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+import java.util.Objects;
+
+@Service
+public class GenimiService implements GenimiServiceImp {
+
+    @Autowired
+    private GenimiConfig genimiConfig;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+
+    @Override
+    public String generateReport(String prompt) {
+        String url = genimiConfig.getUrl() + "?key=" + genimiConfig.getApiKey();
+
+        Map<String, Object> body = Map.of(
+                "Contents", new Object[]{
+                        Map.of("parts", new Object[]{Map.of("text", prompt)})
+                }
+        );
+        return "";
+    }
 }
--- history-analytics-service/src/main/java/com/TaDuy/microservices/history_analytics_service/Service/Imp/GenimiServiceImp.java
+++ history-analytics-service/src/main/java/com/TaDuy/microservices/history_analytics_service/Service/Imp/GenimiServiceImp.java
@@ -1,4 +1,5 @@
 package com.TaDuy.microservices.history_analytics_service.Service.Imp;
 
-public interface GenimiServiceImp {
+public interface GenimiServiceImp  {
+        String generateReport(String prompt);
 }
