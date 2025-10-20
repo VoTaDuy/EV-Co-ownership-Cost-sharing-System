6076e4a93184b40723d64f95c2469c42c09ff6fc
TaiyouDang <kiritanitaiyo@gmail.com>
2025-10-20 19:51:24 +0700

    Add file usage.entity and usage.repository

---
+++ booking-usage-service/src/usage/usage.entity.ts
@@ -0,0 +1,33 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToOne, JoinColumn, OneToOne } from 'typeorm';
+import { Booking } from '../booking/booking.entity';
+
+@Entity({ name: 'usage_record' })
+export class UsageRecord {
+  @PrimaryGeneratedColumn('uuid')
+  usage_id: string;
+
+  @Column()
+  booking_id: string;
+
+  @Column()
+  user_id: string;
+
+  @Column({ type: 'datetime', nullable: true })
+  checkin_time: Date;
+
+  @Column({ type: 'datetime', nullable: true })
+  checkout_time: Date;
+
+  @Column({ type: 'text', nullable: true })
+  vehicle_condition: string;
+
+  @Column({ type: 'float', nullable: true })
+  distance: number;
+
+  @CreateDateColumn({ type: 'datetime', name: 'record_time' })
+  record_time: Date;
+
+  @OneToOne(() => Booking)
+  @JoinColumn({ name: 'booking_id' })
+  booking: Booking;
+}
