dfac74469925d2652e3164f61283c072a6eb394c
TaiyouDang <kiritanitaiyo@gmail.com>
2025-10-22 18:08:16 +0700

    Add file usage.module and update usage module into file app.module

--- booking-usage-service/src/app.module.ts
+++ booking-usage-service/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './config/database.module';
 import { BookingModule } from './booking/booking.module';
+import { UsageModule } from './usage/usage.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { BookingModule } from './booking/booking.module';
     }),
     DatabaseModule,
     BookingModule,
+    UsageModule,
   ],
 })
 export class AppModule {}
---
+++ booking-usage-service/src/usage/usage.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsageRecord } from './usage.entity';
+import { UsageRepository } from './usage.repository';
+import { UsageService } from './usage.service';
+import { UsageController } from './usage.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UsageRecord])],
+  providers: [UsageRepository, UsageService],
+  controllers: [UsageController],
+})
+export class UsageModule {}
