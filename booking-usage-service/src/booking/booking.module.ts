0090d349af614bccc9a439a12eefb679237049fe
TaiyouDang <kiritanitaiyo@gmail.com>
2025-11-01 10:58:42 +0700

    Add file module for conflict log and update file app module and booking module

--- booking-usage-service/src/booking/booking.module.ts
+++ booking-usage-service/src/booking/booking.module.ts
@@ -4,9 +4,12 @@ import { Booking } from './booking.entity';
 import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
 import { BookingRepository } from './booking.repository';
+import { ConflictLogModule } from '../conflict_log/conflict-log.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking])],
+  imports: [TypeOrmModule.forFeature([Booking])
+    , ConflictLogModule
+  ],
   controllers: [BookingController],
   providers: [BookingService, BookingRepository],
   exports: [BookingService],
---
+++ booking-usage-service/src/conflict_log/conflict-log.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConflictLog } from './conflict-log.entity';
+import { ConflictLogRepository } from './conflict-log.repository';
+import { ConflictLogService } from './conflict-log.service';
+import { ConflictLogController } from './conflict-log.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ConflictLog])],
+  controllers: [ConflictLogController],
+  providers: [ConflictLogRepository, ConflictLogService],
+  exports: [ConflictLogService], // Để BookingService có thể inject và tạo conflict
+})
+export class ConflictLogModule {}
