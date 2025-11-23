e35eb1f2d0878e7659b0629e7c6645e31d3bcdc0
Võ Tá Duy <voduystkt@gmail.com>
2025-11-23 20:17:19 +0700

    add new

--- user-and-ownership-service/src/main/java/com/example/EV_Co_ownership/User_and_ownership_service/DTO/ProfileDTO.java
+++ user-and-ownership-service/src/main/java/com/example/EV_Co_ownership/User_and_ownership_service/DTO/ProfileDTO.java
@@ -13,4 +13,60 @@ public class ProfileDTO {
     private String driverLicenseNumber;
     private String driverLicenseExpiry;
     private String licenseImageUrl;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDriverLicenseNumber() {
+        return driverLicenseNumber;
+    }
+
+    public void setDriverLicenseNumber(String driverLicenseNumber) {
+        this.driverLicenseNumber = driverLicenseNumber;
+    }
+
+    public String getDriverLicenseExpiry() {
+        return driverLicenseExpiry;
+    }
+
+    public void setDriverLicenseExpiry(String driverLicenseExpiry) {
+        this.driverLicenseExpiry = driverLicenseExpiry;
+    }
+
+    public String getLicenseImageUrl() {
+        return licenseImageUrl;
+    }
+
+    public void setLicenseImageUrl(String licenseImageUrl) {
+        this.licenseImageUrl = licenseImageUrl;
+    }
 }
