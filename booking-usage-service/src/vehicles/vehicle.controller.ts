1bf8aa753c06498f4d8f5384f96930dc6f50a4c5
TaiyouDang <kiritanitaiyo@gmail.com>
2025-11-26 00:59:26 +0700

    add api get vehicle time line for each group

--- booking-usage-service/src/vehicles/vehicle.controller.ts
+++ booking-usage-service/src/vehicles/vehicle.controller.ts
@@ -1,17 +1,16 @@
 // src/vehicles/vehicle.controller.ts
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query , Param} from '@nestjs/common';
 import { VehicleService } from './vehicle.service';
 
 @Controller('booking/vehicles')
 export class VehicleController {
   constructor(private readonly vehicleService: VehicleService) {}
 
-  @Get('timeline')
-  async getTimeline(@Query('startDate') startDate: string) {
-    if (!startDate) {
-      return { error: 'Thiếu query parameter: startDate' };
-    }
-    const data = await this.vehicleService.getVehicleTimeline(startDate);
-    return { startDate, vehicles: data };
+  @Get(':vehicleId/timeline')
+  async getTimeline(
+    @Param('vehicleId') vehicleId: number,
+    @Query('startDate') startDate: string
+  ) {
+    return this.vehicleService.getVehicleGroupTimeline(vehicleId, startDate);
   }
 }
